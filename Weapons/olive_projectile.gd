3d3cadea870704822178d1537357e3ab882736b5
Eric Vennemeyer <henri.barun@gmail.com>
2025-11-18 14:47:04 -0800

    Update olive projectile to queue_free on contact with another physics body

--- Weapons/olive_projectile.gd
+++ Weapons/olive_projectile.gd
@@ -1,6 +1,14 @@
 extends RigidBody3D
 
+
 var launch_speed: float = 5.0
 
+
 func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	
 	apply_central_impulse(global_basis * Vector3.FORWARD * launch_speed)
+
+
+func _on_body_entered(body: Node) -> void:
+	queue_free()
