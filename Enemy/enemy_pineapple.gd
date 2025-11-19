80d923e0af6de920560874ee1e650c5602c53051
Eric Vennemeyer <henri.barun@gmail.com>
2025-11-19 14:32:41 -0800

    Add random Wander state to EnemyPineapple, add timer for Investigate state, connect together in enum state machine

--- Enemy/enemy_pineapple.gd
+++ Enemy/enemy_pineapple.gd
@@ -3,13 +3,14 @@ extends CharacterBody3D
 
 
 enum EnemyState {
-	Patrolling,
+	Wander,
 	Alerted,
-	Investigating,
+	Investigate,
 	Provoked
 }
 
-@export var investigating_time: float = 2.0
+@export var investigate_duration: float = 2.0
+var investigate_timer: float = 0.0
 
 @export var knockback_force: float = 10.0
 @export var knockback_duration: float = 0.5
@@ -17,6 +18,7 @@ var knockback_timer: float = 0.0
 var knockback_direction: Vector3 = Vector3.ZERO
 
 @export var speed: float = 5.0
+@export var wander_speed: float = 2.0
 @export var aggro_range: float = 12.0
 @export var attack_range: float = 1.5
 @export var max_hitpoints: int = 100
@@ -24,6 +26,7 @@ var knockback_direction: Vector3 = Vector3.ZERO
 
 var player: Player
 var olive_splat_position: Vector3
+var distance_to_player: float
 var provoked: bool = false # provoked is for Player
 var alerted: bool = false # alerted is for Olives
 var hitpoints: int = max_hitpoints:
@@ -38,8 +41,8 @@ var hitpoints: int = max_hitpoints:
 var current_state: EnemyState
 var state_change_timer: float = 0.0
 
-#var target_angle_y: float
-#var rotation_speed: float = 2.0
+var wander_time: float = 0.0
+var wander_direction: Vector3
 
 @onready var temp_body: MeshInstance3D = $TempBody
 @onready var state_label: Label3D = $StateLabel
@@ -49,69 +52,90 @@ var state_change_timer: float = 0.0
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
+	navigation_agent_3d.navigation_finished.connect(_on_navigation_agent_3d_navigation_finished)
 	olive_detector_area_3d.body_entered.connect(_on_olive_detector_body_entered)
-	change_state(EnemyState.Patrolling)
+	change_state(EnemyState.Wander)
 
 
 func change_state(new_state: EnemyState) -> void:
 	current_state = new_state
 	match current_state:
-		EnemyState.Patrolling:
-			state_label.text = "Patrolling"
+		EnemyState.Wander:
+			state_label.text = "Wander"
+			randomize_wander_variables()
 		EnemyState.Alerted:
 			state_label.text = "Alerted"
-		EnemyState.Investigating:
-			state_label.text = "Investigating"
+		EnemyState.Investigate:
+			state_label.text = "Investigate"
+			investigate_timer = investigate_duration
 		EnemyState.Provoked:
 			state_label.text = "Provoked"
 
 
 func _process(delta: float) -> void:
 	match current_state:
+		EnemyState.Wander:
+			if wander_time <= 0.0:
+				randomize_wander_variables()
+			wander_time -= delta
 		EnemyState.Alerted:
 			navigation_agent_3d.target_position = olive_splat_position
+		EnemyState.Investigate:
+			if investigate_timer <= 0.0:
+				change_state(EnemyState.Wander)
+			investigate_timer -= delta
 		EnemyState.Provoked:
 			navigation_agent_3d.target_position = player.global_position
 			olive_splat_position = Vector3.ZERO # remove last saved instance of an olive if enemy notices player
-	
-	## Calculate the current Y-axis rotation
-	#var current_angle = transform.basis.get_euler().y
-	## Smoothly interpolate towards the target angle
-	#var new_angle = lerp_angle(current_angle, target_angle_y, rotation_speed * delta)
-	## Apply the new rotation to the object's transform
-	#transform.basis = Basis.from_euler(Vector3(transform.basis.get_euler().x, new_angle, transform.basis.get_euler().z))
 
 
 func _physics_process(delta: float) -> void:
-	var next_position = navigation_agent_3d.get_next_path_position()
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	var direction = global_position.direction_to(next_position)
-	var distance_to_player = global_position.distance_to(player.global_position)
-	
+	distance_to_player = global_position.distance_to(player.global_position)
 	if distance_to_player <= aggro_range:
 		change_state(EnemyState.Provoked)
 	
-	if current_state == EnemyState.Provoked and distance_to_player <= attack_range:
-		attack()
-	
-	
-	if direction and knockback_timer <= 0.0:
-		look_at_target(direction)
-		#target_angle_y = direction.y
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-
-	if knockback_timer > 0.0:
-		velocity += knockback_direction * knockback_force
-		knockback_timer -= delta
+	match current_state:
+		EnemyState.Wander:
+			velocity = wander_direction * wander_speed
+			look_at_target(wander_direction)
 		
+		EnemyState.Alerted:
+			var next_position = navigation_agent_3d.get_next_path_position()
+			
+			var direction = global_position.direction_to(next_position)
+
+			if direction:
+				look_at_target(direction)
+				velocity.x = direction.x * speed
+				velocity.z = direction.z * speed
+
+		EnemyState.Investigate:
+				velocity.x = move_toward(velocity.x, 0, speed)
+				velocity.z = move_toward(velocity.z, 0, speed)
+		
+		EnemyState.Provoked:
+			if distance_to_player <= attack_range:
+				attack()
+	
+			var next_position = navigation_agent_3d.get_next_path_position()
+			
+			var direction = global_position.direction_to(next_position)
+
+			if direction and knockback_timer <= 0.0:
+				look_at_target(direction)
+				velocity.x = direction.x * speed
+				velocity.z = direction.z * speed
+			else:
+				velocity.x = move_toward(velocity.x, 0, speed)
+				velocity.z = move_toward(velocity.z, 0, speed)
+
+			if knockback_timer > 0.0:
+				velocity += knockback_direction * knockback_force
+				knockback_timer -= delta
 
 	move_and_slide()
 
@@ -122,7 +146,6 @@ func _on_olive_detector_body_entered(body: Node) -> void:
 
 
 func _on_olive_splatted(olive_instance: OliveProjectile) -> void:
-	print("I heard an olive at " + str(olive_instance.global_position))
 	olive_splat_position = olive_instance.global_position
 	change_state(EnemyState.Alerted)
 
@@ -133,6 +156,11 @@ func look_at_target(direction: Vector3) -> void:
 	look_at(global_position + adjusted_direction, Vector3.UP, true)
 
 
+func randomize_wander_variables() -> void:
+	wander_direction = Vector3(randf_range(-1.0, 1.0), 0.0, randf_range(-1.0, 1.0))
+	wander_time = randf_range(1.0, 4.0)
+
+
 func apply_knockback() -> void:
 	knockback_direction = player.global_position.direction_to(global_position).normalized()
 	knockback_direction.y = 0
@@ -141,3 +169,8 @@ func apply_knockback() -> void:
 
 func attack() -> void:
 	player.hitpoints -= attack_damage
+
+
+func _on_navigation_agent_3d_navigation_finished() -> void:
+	if current_state == EnemyState.Alerted:
+		change_state(EnemyState.Investigate)
