1a8c681038846464900f7ee5fa4b7e0609fef099
Eric Vennemeyer <henri.barun@gmail.com>
2025-11-21 11:30:03 -0800

    Replace temp olive and rolling pin with final models. Create rolling pin attack animation and add in temp particles when hitting enemy. Change color/size of olive splat particles.

--- Weapons/melee_weapon.gd
+++ Weapons/melee_weapon.gd
@@ -2,18 +2,15 @@
 extends Node3D
 
 @export var fire_rate: float = 2.0
-#@export var recoil: float = .05
 @export var weapon_mesh: Node3D
 @export var weapon_damage: int = 15
-#@export var muzzle_flash: GPUParticles3D
-#@export var sparks: PackedScene
-#@export var is_automatic: bool = false
+@export var sparks: PackedScene
 @export var ammo_handler: AmmoHandler
 @export var ammo_type: AmmoHandler.ammo_type
 
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
-@onready var weapon_start_position: Vector3 = weapon_mesh.position
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
@@ -24,15 +21,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("Fire"):
 		if cooldown_timer.is_stopped():
 			shoot()
-	#if is_automatic:
-		#if Input.is_action_pressed("Fire"):
-			#if cooldown_timer.is_stopped():
-				#shoot()
-	#else:
-		#if Input.is_action_just_pressed("Fire"):
-			#if cooldown_timer.is_stopped():
-				#shoot()
-	weapon_mesh.position = weapon_mesh.position.lerp(weapon_start_position, delta * 10.0)
 
 
 #func _input(event: InputEvent) -> void:
@@ -40,15 +28,18 @@ func _process(delta: float) -> void:
 
 
 func shoot() -> void:
+	animation_player.play("swing_club")
 	cooldown_timer.start(1.0 / fire_rate)
-	#muzzle_flash.restart()
-	weapon_mesh.position.z -= 1.0
-	var collider = ray_cast_3d.get_collider()
-	printt("Weapon Fired!", collider)
+
+
+
+func do_damage() -> void:
 	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		printt("Weapon Fired!", collider)
 		if collider is EnemyPineapple:
 			collider.hitpoints -= weapon_damage
 			print("Damaged Enemy!")
-		#var spark = sparks.instantiate()
-		#add_child(spark)
-		#spark.global_position = ray_cast_3d.get_collision_point()
+			var spark = sparks.instantiate()
+			add_child(spark)
+			spark.global_position = ray_cast_3d.get_collision_point()
